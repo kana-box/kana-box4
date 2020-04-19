0531c24a5e0e70817884db567a2f8ed4b1c6fd56
kana-box <62703212+kana-box@users.noreply.github.com>
2020-04-19 21:19:44 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -26,4 +26,21 @@ $(window).resize(function(){
   }
 
 });
+
+// modal
+
+  $(".wrapper grid").append("<div id='gray_layer'></div><div id='over_layer'</div>");
+  $("#glay_layer").click(function(){
+    $(this).hide();
+    $("#over_layer").hide();
+  });
+
+  $("a.modal").click(function(){
+    $("#glay_layer").show();
+    $("#over_layer").show().html("<img src="+$(this).attr("href")+">");
+    return false;
+  });
+
+
+
 });
